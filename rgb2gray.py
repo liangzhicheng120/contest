9a9237771ad015887a167a9e1f3d035f255c8377
kimi <liangzhicheng120@126.com>
2017-07-09 21:41:15 +0800

    123

---
+++ rgb2gray.py
@@ -0,0 +1,34 @@
+#coding:utf-8
+from PIL import Image
+from PIL import Image as image
+import numpy as np
+import os
+
+def RGB2Gray(image_origin):
+    gray = image_origin.convert('L')
+    return gray
+
+def saveImage(image_final,i):
+    image_final.save("{0}_gray.png".format(i))
+
+#二值化
+def binaryzation(image_gray):
+    image_gray  = image_gray.resize((120,40),image.ANTIALIAS)
+    image_array = np.array(image_gray)
+    image_array = (image_array/255.0)*(image_array/255.0)
+    #image_array = 1-np.rint(image_array) 
+    image_array = 1-image_array
+    image_array = 1+np.floor(image_array - image_array.mean())
+    return image_array
+
+
+if __name__ == "__main__":
+    for i in range(100000):
+        os.system("clear")
+        print(i)
+        image_handle = Image.open("../image_contest_level_1/{0}.png".format(i))
+        image_handle = RGB2Gray(image_handle)
+        image_array = binaryzation(image_handle)
+        image_array = 255.0*(1-image_array)
+        image_handle = Image.fromarray(np.uint8(image_array))
+        saveImage(image_handle,i)
